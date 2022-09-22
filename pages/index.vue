ddb06933721aee1eeba08e2d028fce9c1e322ce6
ian.liao <ian.liao@ace.io>
2022-09-22 16:56:20 +0800

    [setting] scss style 設定

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,9 @@
   <div class="page-index">
     <!-- config private 測試： {{ API_SECRET }} <br> -->
     <!-- config public  測試： {{ ENV }} <br> -->
+    <div class="aaa">
+      123
+    </div>
     <Logos />
     <Examples />
     <SwitchLanguage />
@@ -16,9 +19,13 @@ const env = { ...runtimeConfig.public }
 console.log(env)
 
 </script>
-<style>
+<style lang="scss" scoped>
+@import '../assets/scss';
 .page-index {
   padding-top: 60px;
   text-align: center;
+  .aaa {
+    color: red;
+  }
 }
 </style>
