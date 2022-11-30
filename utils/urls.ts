32a719be5db46a97798c9e52f7ad638d7cc47f26
ian.liao <ian.liao@ace.io>
2022-11-30 11:07:09 +0800

    fix: urls (prod)

--- utils/urls.ts
+++ utils/urls.ts
@@ -2,30 +2,31 @@
 export function getURLs () {
   const localeSetting = useState<string>('locale.setting')
   const lang = localeSetting.value
+  const isProduction = process.env.NODE_ENV === 'production'
   return {
-    login: `/ex/${lang}/login`,
-    signUp: `/ex/${lang}/register`,
+    login: isProduction ? '/login' : `/ex/${lang}/login`,
+    signUp: isProduction ? '/register' : `/ex/${lang}/register`,
     // 幣幣交易
-    trade: `/ex/${lang}/trade`,
+    trade: isProduction ? '/trade' : `/ex/${lang}/trade`,
     // c2c交易
     c2c: `/otc/${lang}`,
     // 訂單
     orders: {
       // 幣幣訂單
-      trade: `/co/${lang}/order/exchangeOrder`,
+      trade: isProduction ? '/order/exchangeOrder' : `/co/${lang}/order/exchangeOrder`,
       // c2c訂單
-      c2c: `/co/${lang}/order/otcOrder`,
+      c2c: isProduction ? '/order/otcOrder' : `/co/${lang}/order/otcOrder`,
       // 合約訂單
-      co: `/co/${lang}/trade/order/coOrder`
+      co: isProduction ? '/order/coOrder' : `/co/${lang}/order/coOrder`
     },
     // 資產
     assets: {
       // 幣幣資產
-      trade: `/co/${lang}/assets/exchangeOrder`,
+      trade: isProduction ? '/assets/exchangeAccount' : `/co/${lang}/assets/exchangeAccount`,
       // c2c資產
-      c2c: `/co/${lang}/assets/otcAccount`,
+      c2c: isProduction ? '/assets/otcAccount' : `/co/${lang}/assets/otcAccount`,
       // 合約資產
-      co: `/co/${lang}/trade/assets/coAccount`
+      co: isProduction ? '/assets/coAccount' : `/co/${lang}/assets/coAccount`
     },
     AGET: 'https://aget.aceglobal.io/'
   }
