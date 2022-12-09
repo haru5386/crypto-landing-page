5829ff3bd6555d8edd2658c9fb0536518dfaf46d
ian.liao <ian.liao@ace.io>
2022-12-09 15:41:06 +0800

    fix: 導頁路徑新增語系

--- utils/urls.ts
+++ utils/urls.ts
@@ -4,29 +4,29 @@ export function getURLs () {
   const lang = localeSetting.value
   const isProduction = process.env.NODE_ENV === 'production'
   return {
-    login: isProduction ? '/login' : `/ex/${lang}/login`,
-    signUp: isProduction ? '/register' : `/ex/${lang}/register`,
+    login: isProduction ? `/${lang}/login` : `/ex/${lang}/login`,
+    signUp: isProduction ? `/${lang}/register` : `/ex/${lang}/register`,
     // 幣幣交易
-    trade: isProduction ? '/trade' : `/ex/${lang}/trade`,
+    trade: isProduction ? `/${lang}/trade` : `/ex/${lang}/trade`,
     // c2c交易
     c2c: `/otc/${lang}`,
     // 訂單
     orders: {
       // 幣幣訂單
-      trade: isProduction ? '/order/exchangeOrder' : `/co/${lang}/order/exchangeOrder`,
+      trade: isProduction ? `/${lang}/order/exchangeOrder` : `/co/${lang}/order/exchangeOrder`,
       // c2c訂單
-      c2c: isProduction ? '/order/otcOrder' : `/co/${lang}/order/otcOrder`,
+      c2c: isProduction ? `/${lang}/order/otcOrder` : `/co/${lang}/order/otcOrder`,
       // 合約訂單
-      co: isProduction ? '/order/coOrder' : `/co/${lang}/order/coOrder`
+      co: isProduction ? `/${lang}/order/coOrder` : `/co/${lang}/order/coOrder`
     },
     // 資產
     assets: {
       // 幣幣資產
-      trade: isProduction ? '/assets/exchangeAccount' : `/co/${lang}/assets/exchangeAccount`,
+      trade: isProduction ? `/${lang}/assets/exchangeAccount` : `/co/${lang}/assets/exchangeAccount`,
       // c2c資產
-      c2c: isProduction ? '/assets/otcAccount' : `/co/${lang}/assets/otcAccount`,
+      c2c: isProduction ? `/${lang}/assets/otcAccount` : `/co/${lang}/assets/otcAccount`,
       // 合約資產
-      co: isProduction ? '/assets/coAccount' : `/co/${lang}/assets/coAccount`
+      co: isProduction ? `/${lang}/assets/coAccount` : `/co/${lang}/assets/coAccount`
     },
     AGET: 'https://aget.aceglobal.io/'
   }
